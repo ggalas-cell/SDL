5a0383b55c3441fe4b8363bd547c2dc26b93d30b
ggalas-cell <ggalas@gaming.tech>
2025-12-02 12:58:42 +0100

    feat : circle

--- SDL/Circle.h
+++ SDL/Circle.h
@@ -3,6 +3,7 @@
 class Circle: public Geometry
 {
 public:
+	Circle(int cx,int cy,int rad, int pre ) : Geometry(cx,cy,rad,pre){}
 	void Draw(SDL_Renderer* renderer, int centerX, int centerY, int radius, int precision);
 };
 
--- SDL/Rectangle.h
+++ SDL/Rectangle.h
@@ -2,8 +2,6 @@
 #include "Geometry.h"
 class Rectangle: public Geometry
 {
-private:
-	int mr_width, mr_height;
 public:	
 	Rectangle(int x,int y,int w, int h) : Geometry(x,y,w,h) { }
 	void Draw(SDL_Renderer* renderer, int x, int y, int width, int height);
