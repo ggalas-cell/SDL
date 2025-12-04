e57fef0b4ad5f82e10a536cf4c590816fb3d6600
ggalas-cell <ggalas@gaming.tech>
2025-12-04 12:59:08 +0100

    feat : rename

--- SDL/Rectangle.cpp
+++ SDL/Rectangle.cpp
@@ -6,10 +6,6 @@ void Rectangle::Draw(SDL_Renderer* renderer)
 	//SDL_RenderDrawLine(renderer, m_Posx + m_width, m_Posy, m_Posx + m_width, m_height + m_Posy);
 	//SDL_RenderDrawLine(renderer, m_Posx, m_Posy, m_Posx, m_height + m_Posy);
 	//SDL_RenderDrawLine(renderer, m_Posx, m_Posy + m_height, m_Posx + m_width, m_Posy + m_height);
-	SDL_Rect rect;
-	rect.x = m_Posx; 
-	rect.y = m_Posy; 
-	rect.w = m_width;
-	rect.h = m_height;
+	SDL_Rect rect = {m_Posx,m_Posy,m_width,m_height};
 	SDL_RenderDrawRect(renderer, &rect);
 }
--- SDL/main.cpp
+++ SDL/main.cpp
@@ -110,14 +110,14 @@ int main(int arcg, char* argv[])
 	//DrawPoint(renderer,color);
 	//DrawHorizontalLine(renderer, 10, 10, 100);	
 	//DrawVerticalLine(renderer,50,50,200);
-	//DrawRectangle(renderer,50,50,150,100);
+	DrawRectangle(renderer,50,50,150,100);
 	//DrawCircle(renderer, 200, 200, 200, 2000);
 	//DrawLine(renderer,200,100,400,300);
 
-	Rectangle* rectangle= new Rectangle(100,100);
-	// Rectangle* rectangle2= new Rectangle(100,100);
-	rectangle->SetPosition(windx, windy, 0.5f, 0.5f);
-	rectangle->Draw(renderer);
+	//Rectangle* rectangle= new Rectangle(100,100);
+	//// Rectangle* rectangle2= new Rectangle(100,100);
+	//rectangle->SetPosition(windx, windy, 0.5f, 0.5f);
+	//rectangle->Draw(renderer);
 
 	SDL_RenderPresent(renderer);
 	SDL_Delay(2000);
